f7ad143d278d5e8f090b4e09e4e128461eec7387
Shanea-Jaromay1 <shanea.jaromay@stu.ocsb.ca>
2025-04-02 17:16:13 +0000

    init

--- js/script.js
+++ js/script.js
@@ -1,5 +1,21 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2025 Shanea Jaromay All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Shanea Jaromay
+// Created on: Apr 2025
 // This file contains the JS functions for index.html
+
+"use strict"
+
+/**
+ * Show volume
+ */
+function calculateVolumeOfSphere() {
+  // input
+  const radiusNumber = parseFloat(document.getElementById("radius-number").value);
+
+  // process
+  const volumeOfSphere = (4/3) * Math.PI * (radiusNumber ** 3);
+
+  // output with more controlled precision
+  document.getElementById("volume").innerHTML = "The volume is:" + volumeOfSphere.toFixed(2) + "mm³";
+}
